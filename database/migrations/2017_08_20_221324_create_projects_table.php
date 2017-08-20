25183424e8ed2763fa854a84ee76043f9c5d3429
Joel de Leon <joel.dl08@gmail.com>
2017-08-20 17:39:30 -0500

    Created projects migration, model and controller

---
+++ database/migrations/2017_08_20_221324_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->number('quotes');
+            $table->datetime('start_date');
+            $table->datetime('delivery_date');
+            $table->datetime('estimated_delivery_date');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
