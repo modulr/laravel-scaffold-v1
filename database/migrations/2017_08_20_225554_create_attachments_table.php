d4562f30f4959fed5eded55628f627c1dfed7fd9
jose manuel escalante torres <josemanuel.0095@gmail.com>
2017-08-20 18:15:44 -0500

    Created migration, model and controller for quotes, employee and attachtment

---
+++ database/migrations/2017_08_20_225554_create_attachments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('attachments', function (Blueprint $table) {
+          $table->increments('id');
+          $table->foreign('quote_id')->references('id')->on('quotes');
+          $table->text('route');
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
