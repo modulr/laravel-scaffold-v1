9102f136d26a8a0f8ec0e4fe46726140984a1eda
alfredobarron <alfredobarronc@gmail.com>
2017-08-24 12:35:03 -0500

    Add table project_status

--- database/migrations/2017_08_20_170857_project_status_table.php
+++ database/migrations/2017_08_20_170857_project_status_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjectsTable extends Migration
+class ProjectStatusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateProjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function(Blueprint $table) {
+        Schema::create('project_status', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->datetime('start_date');
-            $table->datetime('delivery_date');
-            $table->integer('status_id')->unsigned();
-            $table->foreign('status_id')->references('id')->on('project_status');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -32,6 +28,6 @@ class CreateProjectsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('project_status');
     }
 }
