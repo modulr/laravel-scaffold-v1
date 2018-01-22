7f9b13c567acc3bde007fc0ae3ff64d706c0a8a7
deleonn <joel.dl08@gmail.com>
2018-01-22 10:57:59 -0600

    Widget fix, now getting records by current year

--- app/Http/Controllers/WidgetController.php
+++ app/Http/Controllers/WidgetController.php
@@ -18,15 +18,21 @@ class WidgetController extends Controller
     public function getMonthOpportunities()
     {
         $currentMonth = date('m');
-        return Project::where('status', 1)->whereRaw('MONTH(start_date) = ?', [$currentMonth])->count();
+        $currentYear = date('Y');
+
+        return Project::where('status', 1)
+        ->whereYear('registered_date', $currentYear)
+        ->whereRaw('MONTH(registered_date) = ?', [$currentMonth])
+        ->count();
     }
 
     public function getOpportunityInsights()
     {
-        $approvedOpportunities = Project::where('status', 2)->get();
-        $activeOpportunities = Project::where('status', 1)->get();
-        $rejectedOpportunities = Project::where('status', 0)->get();
-        $allOpportunities = Project::all();
+        $currentYear = date('Y');
+        $approvedOpportunities = Project::where('status', 2)->whereYear('registered_date', $currentYear)->get();
+        $activeOpportunities = Project::where('status', 1)->whereYear('registered_date', $currentYear)->get();
+        $rejectedOpportunities = Project::where('status', 0)->whereYear('registered_date', $currentYear)->get();
+        $allOpportunities = Project::whereYear('registered_date', $currentYear)->get();
 
         return response()->json([
             'approved' => $approvedOpportunities,
@@ -39,16 +45,23 @@ class WidgetController extends Controller
     public function opportunityCharts()
     {
         $activeArray = array();
+        $currentYear = date('Y');
 
         for ($i=1; $i <= 12; $i++) {
-            $activeData = Project::where('status', 1)->whereRaw('MONTH(registered_date) = ?' , $i)->get();
+            $activeData = Project::where('status', 1)
+                ->whereYear('registered_date', $currentYear)
+                ->whereRaw('MONTH(registered_date) = ?' , $i)
+                ->get();
             $activeArray[$i] = $activeData->count();
         }
 
         $rejectedArray = array();
 
         for ($i=1; $i <= 12; $i++) {
-            $rejectedData = Project::where('status', 0)->whereRaw('MONTH(registered_date) = ?' , $i)->get();
+            $rejectedData = Project::where('status', 0)
+                ->whereYear('registered_date', $currentYear)
+                ->whereRaw('MONTH(registered_date) = ?' , $i)
+                ->get();
             $rejectedArray[$i] = $rejectedData->count();
         }
 
@@ -63,16 +76,23 @@ class WidgetController extends Controller
     public function getMonthQuotes()
     {
         $currentMonth = date('m');
-        return Quotes::where('status', 1)->whereRaw('MONTH(start_date) = ?', [$currentMonth])->count();
+        $currentYear = date('Y');
+
+        return Quotes::where('status', 1)
+        ->whereYear('request_date', $currentYear)
+        ->whereRaw('MONTH(request_date) = ?', [$currentMonth])
+        ->count();
     }
 
     public function getQuoteInsights()
     {
-        $activeQuotes = Quote::whereIn('status_id', [1, 2, 3])->get();
-        $rejectedQuotes = Quote::where('status_id', 4)->get();
-        $getTotalMXN = Quote::where('currency_id', 1)->sum('amount');
-        $getTotalUSD = Quote::where('currency_id', 2)->sum('amount');
-        $allQuotes = Quote::all();
+        $currentYear = date('Y');
+        
+        $activeQuotes = Quote::whereIn('status_id', [1, 2, 3])->whereYear('request_date', $currentYear)->get();
+        $rejectedQuotes = Quote::where('status_id', 4)->whereYear('request_date', $currentYear)->get();
+        $getTotalMXN = Quote::where('currency_id', 1)->whereYear('request_date', $currentYear)->sum('amount');
+        $getTotalUSD = Quote::where('currency_id', 2)->whereYear('request_date', $currentYear)->sum('amount');
+        $allQuotes = Quote::whereYear('request_date', $currentYear)->get();
 
         return response()->json([
             'totalMXN' => $getTotalMXN,
@@ -86,9 +106,12 @@ class WidgetController extends Controller
     public function quoteCharts()
     {
         $activeArray = array();
-
+        $currentYear = date('Y');
         for ($i=1; $i <= 12; $i++) {
-            $activeData = Quote::whereIn('status_id', [1, 2, 3])->whereRaw('MONTH(request_date) = ?' , $i)->get();
+            $activeData = Quote::whereIn('status_id', [1, 2, 3])
+                ->whereYear('request_date', $currentYear)
+                ->whereRaw('MONTH(request_date) = ?' , $i)
+                ->get();
             if ($activeData->count() != 0) {
                 $activeArray[$i] = $activeData->count();
             } else {
@@ -99,7 +122,10 @@ class WidgetController extends Controller
         $rejectedArray = array();
 
         for ($i=1; $i <= 12; $i++) {
-            $rejectedData = Quote::where('status_id', 4)->whereRaw('MONTH(request_date) = ?' , $i)->get();
+            $rejectedData = Quote::where('status_id', 4)
+                ->whereYear('request_date', $currentYear)
+                ->whereRaw('MONTH(request_date) = ?' , $i)
+                ->get();
             if ($rejectedData->count() != 0) {
                 $rejectedArray[$i] = $rejectedData->count();
             } else {
