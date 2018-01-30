723c73f9000bd6b8dc6b52a549d6b8e224e4be1a
alfredobarron <alfredobarronc@gmail.com>
2018-01-30 16:31:23 -0600

    Auth -> update links modules images

--- resources/assets/js/components/auth/Carousel.vue
+++ resources/assets/js/components/auth/Carousel.vue
@@ -2,25 +2,16 @@
     <swiper :options="swiperOption" ref="mySwiper">
         <swiper-slide>
             <div class="text-center">
-                <img src="http://modulr.io/img/modules/notifications2.png" class="img-responsive">
+                <img src="http://modulr.io/img/modules/auth.png" class="img-responsive">
                 <h3>
-                    <i class="mdi mdi-notifications mdi-lg text-primary"></i> Notifications
+                    <i class="mdi mdi-lock-outline mdi-lg text-primary"></i> Authentication
                 </h3>
-                <p>You will know when there is news</p>
+                <p>Sign Up, login or recover your lost password</p>
             </div>
         </swiper-slide>
         <swiper-slide>
             <div class="text-center">
-                <img src="http://modulr.io/img/modules/news2.png" class="img-responsive">
-                <h3>
-                    <i class="mdi mdi-whatshot mdi-lg text-primary"></i> News
-                </h3>
-                <p>Keep up to date</p>
-            </div>
-        </swiper-slide>
-        <swiper-slide>
-            <div class="text-center">
-                <img src="http://modulr.io/img/modules/contacts2.png" class="img-responsive">
+                <img src="http://modulr.io/img/modules/contacts.png" class="img-responsive">
                 <h3>
                     <i class="mdi mdi-contacts mdi-lg text-primary"></i> Contacts
                 </h3>
@@ -29,7 +20,34 @@
         </swiper-slide>
         <swiper-slide>
             <div class="text-center">
-                <img src="http://modulr.io/img/modules/tasks2.png" class="img-responsive">
+                <img src="http://modulr.io/img/modules/files.png" class="img-responsive">
+                <h3>
+                    <i class="mdi mdi-folder-open mdi-lg text-primary"></i> Files
+                </h3>
+                <p>Manage your files and share it</p>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="text-center">
+                <img src="http://modulr.io/img/modules/news.png" class="img-responsive">
+                <h3>
+                    <i class="mdi mdi-whatshot mdi-lg text-primary"></i> News
+                </h3>
+                <p>Keep up to date</p>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="text-center">
+                <img src="http://modulr.io/img/modules/notifications.png" class="img-responsive">
+                <h3>
+                    <i class="mdi mdi-notifications mdi-lg text-primary"></i> Notifications
+                </h3>
+                <p>You will know when there is news</p>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="text-center">
+                <img src="http://modulr.io/img/modules/tasks.png" class="img-responsive">
                 <h3>
                     <i class="mdi mdi-view-list mdi-lg text-primary"></i> Tasks
                 </h3>
@@ -38,11 +56,11 @@
         </swiper-slide>
         <swiper-slide>
             <div class="text-center">
-                <img src="http://modulr.io/img/modules/files2.png" class="img-responsive">
+                <img src="http://modulr.io/img/modules/users.png" class="img-responsive">
                 <h3>
-                    <i class="mdi mdi-folder-open mdi-lg text-primary"></i> Files
+                    <i class="mdi mdi-vpn-key mdi-lg text-primary"></i> Users & Access
                 </h3>
-                <p>Manage your files and share it</p>
+                <p>Access control to users and permissions</p>
             </div>
         </swiper-slide>
         <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
