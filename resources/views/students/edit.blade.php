1caf42995d6aa0e076e66619ae2db8ec6a955700
alfredobarron <alfredobarronc@gmail.com>
2018-03-08 16:46:15 -0600

    p-uc -> remove container-fluid

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -1,12 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <students-edit :id="{{$id}}"></students-edit>
-            </div>
-          </div>
-        </div>
-    </div>
+    <students-edit :id="{{$id}}"></students-edit>
 @endsection
--- resources/views/students/list.blade.php
+++ resources/views/students/list.blade.php
@@ -1,12 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <students-list></students-list>
-            </div>
-          </div>
-        </div>
-    </div>
+    <students-list></students-list>
 @endsection
